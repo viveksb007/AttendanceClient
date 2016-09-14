e7f67ece56a882a847b3e15e6386b905b40de84d
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2016-09-14 19:53:14 +0530

    Yet to Test

--- app/src/main/java/com/viveksb007/attendenceclient/MainActivity.java
+++ app/src/main/java/com/viveksb007/attendenceclient/MainActivity.java
@@ -1,87 +1,143 @@
 package com.viveksb007.attendenceclient;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private final String TAG = getClass().getSimpleName();
-    Button btnScanAP;
-    TextView tvAP;
-    WifiManager wifiManager;
-    ScanReceiver scanReceiver;
-    StringBuilder sb = new StringBuilder();
+    TextView textResponse;
+    EditText editTextAddress, editTextPort;
+    Button btnMarkAttendence;
+    String MAC_ADDRESS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnScanAP = (Button) findViewById(R.id.button);
-        btnScanAP.setOnClickListener(this);
-        tvAP = (TextView) findViewById(R.id.tv_scan_result);
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        MAC_ADDRESS = wifiInfo.getMacAddress();
 
-        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        scanReceiver = new ScanReceiver();
+        editTextAddress = (EditText) findViewById(R.id.address);
+        editTextPort = (EditText) findViewById(R.id.port);
+        btnMarkAttendence = (Button) findViewById(R.id.connect);
+        textResponse = (TextView) findViewById(R.id.response);
 
-        if (!wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(true);
+        btnMarkAttendence.setOnClickListener(buttonConnectOnClickListener);
+    }
+
+    View.OnClickListener buttonConnectOnClickListener =
+            new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+                    MyClientTask myClientTask = new MyClientTask(
+                            editTextAddress.getText().toString(),
+                            Integer.parseInt(editTextPort.getText().toString()));
+                    myClientTask.execute();
+                }
+            };
+
+    public class MyClientTask extends AsyncTask<Void, Void, Void> {
+
+        String dstAddress;
+        int dstPort;
+        String response = "";
+
+        MyClientTask(String addr, int port) {
+            dstAddress = addr;
+            dstPort = port;
         }
-        wifiManager.startScan();
-    }
-
-    @Override
-    protected void onResume() {
-        registerReceiver(scanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        unregisterReceiver(scanReceiver);
-        super.onPause();
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                break;
-        }
-    }
-
-
-    private class ScanReceiver extends BroadcastReceiver {
 
         @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.v(TAG, "Received");
-            List<ScanResult> wifiScanList = wifiManager.getScanResults();
-            sb = new StringBuilder();
-            sb.append("\n        Number Of Wifi connections :" + wifiScanList.size() + "\n\n");
+        protected Void doInBackground(Void... arg0) {
 
-            for (int i = 0; i < wifiScanList.size(); i++) {
-                sb.append(new Integer(i + 1).toString() + ". ");
-                sb.append((wifiScanList.get(i)).toString());
-                sb.append("\n\n");
+            Socket socket = null;
+            DataOutputStream dataOutputStream = null;
+            DataInputStream dataInputStream = null;
+
+            try {
+                socket = new Socket(dstAddress, dstPort);
+
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+
+                if (MAC_ADDRESS != null) {
+                    dataOutputStream.writeUTF(MAC_ADDRESS);
+                }
+
+                response = dataInputStream.readUTF();
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                response = "UnknownHostException: " + e.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                response = "IOException: " + e.toString();
+            } finally {
+
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
-            tvAP.setText(sb);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            if (response == "404") {
+                textResponse.setText(response + "Not Marked");
+            } else if(response == "200") {
+                textResponse.setText("Your attendence is Marked :D");
+                stopWifi();
+            }else {
+                textResponse.setText(response);
+            }
+            super.onPostExecute(result);
+        }
+
+    }
+
+    public WifiManager mWifiManager;
+
+    private void stopWifi() {
+        mWifiManager = (WifiManager) getBaseContext().getSystemService(Context.WIFI_SERVICE);
+        if (mWifiManager.isWifiEnabled()) {
+            mWifiManager.setWifiEnabled(false);
         }
     }
 }
